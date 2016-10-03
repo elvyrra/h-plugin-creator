2ede1605a31c224ba25fccdf175848dd2a4a5b31
Sebastien Lecocq <lecocq.elvyrra@gmail.com>
2016-10-03 14:23:14 +0200

    update EMV version + fix icons selectbox issue

--- lib/IconSelector.php
+++ lib/IconSelector.php
@@ -8,7 +8,7 @@ class IconSelector {
 
         $allIcons = array();
 
-        preg_match_all('/\.icon-([^\:]+)\:before\{content\: "(.+?)"\}/', $css, $matches, PREG_SET_ORDER);
+        preg_match_all('/\.icon-([^\:]+)\:before\s+\{\s+content\: "(.+?)"\;\s+\}/s', $css, $matches, PREG_SET_ORDER);
 
         foreach($matches as $match) {
             $allIcons[$match[1]] = substr($match[2], 1);
